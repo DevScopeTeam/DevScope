a45e91df41f3a38e3e7324f1031da17598a27799
Morri3 <824440291@qq.com>
2024-10-29 00:22:42 +0800

    feat: add some dependencies, install fonts and developing homepage

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,110 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import TitleLogo from './components/TitleLogo.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="outer_box">
+    <TitleLogo class="title"/>
+  </div>
+  
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+<style lang="scss" scoped>
+.outer_box{
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+  height: 100%;
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+  // 子元素垂直水平居中
+  position: relative;
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
+  .title{
+    width: 100%;
+    height: 100%;
 
-nav a:first-of-type {
-  border: 0;
-}
+    // 居中
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+    font-family: TsangerYuYangT_W05_W05;
+    font-size: 60px;
+    color: rgb(85, 158, 237);
+    font-style: italic;
+    letter-spacing: 3px;
   }
 }
+
+
+// header {
+//   line-height: 1.5;
+//   max-height: 100vh;
+// }
+
+// .logo {
+//   display: block;
+//   margin: 0 auto 2rem;
+// }
+
+// nav {
+//   width: 100%;
+//   font-size: 12px;
+//   text-align: center;
+//   margin-top: 2rem;
+// }
+
+// nav a.router-link-exact-active {
+//   color: var(--color-text);
+// }
+
+// nav a.router-link-exact-active:hover {
+//   background-color: transparent;
+// }
+
+// nav a {
+//   display: inline-block;
+//   padding: 0 1rem;
+//   border-left: 1px solid var(--color-border);
+// }
+
+// nav a:first-of-type {
+//   border: 0;
+// }
+
+// @media (min-width: 1024px) {
+//   header {
+//     display: flex;
+//     place-items: center;
+//     padding-right: calc(var(--section-gap) / 2);
+//   }
+
+//   .logo {
+//     margin: 0 2rem 0 0;
+//   }
+
+//   header .wrapper {
+//     display: flex;
+//     place-items: flex-start;
+//     flex-wrap: wrap;
+//   }
+
+//   nav {
+//     text-align: left;
+//     margin-left: -1rem;
+//     font-size: 1rem;
+
+//     padding: 1rem 0;
+//     margin-top: 1rem;
+//   }
+// }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import ElementPlus from 'element-plus' // import elementPlus
+import 'element-plus/dist/index.css' // import elementPlus
+import '@/assets/index.scss'// import fonts
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
