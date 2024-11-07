c0016323563cf5e43be5c2ef291a170029179ec7
zjy2414 <zjy2414@outlook.com>
2024-11-07 09:50:15 +0800

    fix: nation_data

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -36,7 +36,7 @@ const state = reactive({
 })
 
 // const goSearch = async () => {
-async function goSearch () {
+async function goSearch() {
   // console.log('current search content: ' + searchStore.getSearchContent()) // username / nation
   // console.log('current field content: ' + state.uuid)
 
@@ -44,7 +44,7 @@ async function goSearch () {
   if (searchStore.getSearchMode() === true) {
     // 领域搜索
 
-    if(state.uuid === ''){
+    if (state.uuid === '') {
       ElNotification({
         title: 'Attention',
         message: 'Please enter the content to search!',
@@ -57,8 +57,8 @@ async function goSearch () {
       const [err, data] = await api.listDomainRank(state.uuid, 1, 100)
       if (err) handleNetworkError(err)
       if (!data || !data?.list) return
-      let rankList = data.list
-      
+      const rankList = data.list
+
       // 2.若有选择nation，则从step2的返回结果中筛选指定nation的用户
       let tmpList = reactive<DeveloperRank[]>([])
       if (searchStore.checkIfEmpty()) { // 输入nation搜索
@@ -101,6 +101,9 @@ async function goSearch () {
 
       const [err2, nation_data] = await api.getNation(searchStore.state.searchContent)
       if (err2) handleNetworkError(err2)
+      if (nation_data?.nation) {
+        api_user.location = nation_data.nation;
+      }
       if (api_user.location == "" || api_user.location == "Unknown") {
         api_user.location = "N/A"
       }
@@ -116,7 +119,7 @@ async function goSearch () {
       // 往userStore.ts更新获取的user信息
       userStore.setUserInfo(JSON.stringify(api_user))
       console.log(userStore.getUserInfo())
-      
+
       // 3.jump to the ListView
       router.push({
         path: '/list'
@@ -138,41 +141,37 @@ async function goSearch () {
   <div class="outer_box">
     <!-- 领域搜索 -->
     <div v-if="searchStore.getSearchMode()" class="input_box">
-      <el-input class="input" 
-        :style="{width:props.inputWidth+props.inputWidthUnit, height:props.inputHeight+props.inputHeightUnit}"
-        placeholder="Enter a nation."
-        v-model="searchStore.state.searchContent">
+      <el-input class="input"
+        :style="{ width: props.inputWidth + props.inputWidthUnit, height: props.inputHeight + props.inputHeightUnit }"
+        placeholder="Enter a nation." v-model="searchStore.state.searchContent">
       </el-input>
 
       <el-select class="field"
-        :style="{width:(100-15-props.inputWidth)+props.inputWidthUnit, height:props.inputHeight+props.inputHeightUnit}"  
-        v-model="state.uuid"
-        placeholder="Choose a field."
-        clearable
-        ref="category">
-        <el-option v-for="(item, index) in fieldStore.getFieldList()" :key="index" :label="item.name" :value="item.uuid"/>
+        :style="{ width: (100 - 15 - props.inputWidth) + props.inputWidthUnit, height: props.inputHeight + props.inputHeightUnit }"
+        v-model="state.uuid" placeholder="Choose a field." clearable ref="category">
+        <el-option v-for="(item, index) in fieldStore.getFieldList()" :key="index" :label="item.name"
+          :value="item.uuid" />
       </el-select>
     </div>
 
     <!-- 普通搜索 -->
     <div v-else class="input_box">
-      <el-input class="input" 
-        :style="{width:props.inputWidth+props.inputWidthUnit, height:props.inputHeight+props.inputHeightUnit}"
-        placeholder="Enter an username."
-        v-model="searchStore.state.searchContent">
+      <el-input class="input"
+        :style="{ width: props.inputWidth + props.inputWidthUnit, height: props.inputHeight + props.inputHeightUnit }"
+        placeholder="Enter an username." v-model="searchStore.state.searchContent">
       </el-input>
     </div>
 
     <!-- 共用的搜索图标 -->
     <div class="button">
-      <img class="icon" :style="{width:props.iconWidth+'px', height:props.iconHeight+'px'}" 
-        :src="props.image===0 ? search : search_black" alt="" @click="goSearch()"/>
+      <img class="icon" :style="{ width: props.iconWidth + 'px', height: props.iconHeight + 'px' }"
+        :src="props.image === 0 ? search : search_black" alt="" @click="goSearch()" />
     </div>
   </div>
 </template>
 
 <style scoped>
-.outer_box{
+.outer_box {
   width: 100%;
   height: 100%;
 
@@ -183,7 +182,7 @@ async function goSearch () {
   align-items: center;
 
   /* 按领域搜索的输入框 */
-  .input_box{
+  .input_box {
     width: 100%;
     height: 100%;
 
@@ -192,12 +191,13 @@ async function goSearch () {
     justify-content: center;
     align-items: center;
 
-    .input{
+    .input {
       padding-right: 10px;
     }
 
     /* 自定义el-select内部样式 */
-    :deep(.el-select__wrapper) { /* 外框 */
+    :deep(.el-select__wrapper) {
+      /* 外框 */
       height: 60px;
 
       border-radius: 40px;
@@ -209,10 +209,13 @@ async function goSearch () {
   }
 
   /* 自定义el-input内部样式 */
-  :deep(.el-input__wrapper) { /* 外框 */
+  :deep(.el-input__wrapper) {
+    /* 外框 */
     border-radius: 40px;
   }
-  :deep(.el-input__inner) { /* 输入区域 */
+
+  :deep(.el-input__inner) {
+    /* 输入区域 */
     margin: 0 10px;
     font-size: 16px;
     letter-spacing: 2px;
@@ -220,7 +223,7 @@ async function goSearch () {
   }
 
   /* 搜索图标 */
-  .button{
+  .button {
     width: 15%;
     height: 100%;
 
@@ -229,11 +232,11 @@ async function goSearch () {
     justify-content: center;
     align-items: center;
 
-    .icon{
+    .icon {
       margin-left: 5px;
     }
 
-    .icon:hover{
+    .icon:hover {
       cursor: pointer;
     }
   }
